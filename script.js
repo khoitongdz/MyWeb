8c6ea2dfeec0d8d240a8e4662227853931db93db
Khoitongdz <khoitong0000@gmail.com>
2025-03-30 10:26:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -39,8 +39,20 @@ document.addEventListener('DOMContentLoaded', function() {
                 url: 'https://webproxy.proxyshare.com/request?p=GgkeGkYuKF0ORhMPDwsIQVRUFRQMVRwcVBoLCwhUHBoXGgMCVgsXGgJWDx4YExUUFxQcAlYXEhYSDx4fVEpLTkpJVA8THlYMGhcQEhUcVh8eGh9WCA4JDRINFAkIVRMPFhc='
             },
             {
+                logo: 'https://cdn.now.gg/assets-opt/_next/image?url=https%3A%2F%2Fcdn.now.gg%2Fassets-opt%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fcdn.now.gg%252Fapps-content%252F8796%252Ficon%252Fmagic-chronicle-isekai-rpg.png%26w%3D256%26q%3D80&w=1440&q=70',
+                url: 'https://webproxy.proxyshare.com/request?p=GgkeGkYuKF0ORhMPDwsIQVRUFRQMVRwcVBoLCwhUFhocEhhWHBoWHlRDTEJNVBYaHBIYVhgTCRQVEhgXHlYSCB4QGhJWCQscVRMPFhc='
+            },
+            {
                 logo: 'https://cdn.now.gg/apps-content/10417/icon/chasers.png',
                 url: 'https://webproxy.proxyshare.com/request?p=GgkeGkYuKF0ORhMPDwsIQVRUFRQMVRwcVBoLCwhUFxIdDhcIHlRKS09KTFQYExoIHgkIVRMPFhc='
+            },
+            {
+                logo: 'https://cdn.now.gg/apps-content/8861/icon/shadow-fight-3.png',
+                url: 'https://webproxy.proxyshare.com/request?p=GgkeGkYuKF0ORhMPDwsIQVRUFRQMVRwcVBoLCwhUFR4QEBJUQ0NNSlQIExofFAxWHRIcEw9WSFUTDxYX'
+            },
+            {
+                logo: 'https://cdn.now.gg/assets-opt/_next/image?url=https%3A%2F%2Fcdn.now.gg%2Fassets-opt%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fcdn.now.gg%252Fapps-content%252F8888%252Ficon%252Fzombieland-doomsday-survival.png%26w%3D256%26q%3D80&w=1440&q=70',
+                url: 'https://webproxy.proxyshare.com/request?p=GgkeGkYuKF0ORhMPDwsIQVRUFRQMVRwcVBoLCwhUEQ4eHR4VHFYcGhYeCFRDQ0NDVAEUFhkSHhcaFR9WHxQUFggfGgJWCA4JDRINGhdVEw8WFw=='
             }
         ];
 
@@ -70,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             {
                 logo: 'https://cdn.now.gg/assets-opt/_next/image?url=https%3A%2F%2Fcdn.now.gg%2Fassets-opt%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fcdn.now.gg%252Fapps-content%252Fcom.roblox.client%252Ficon%252Froblox.png%26w%3D256%26q%3D80&w=1440&q=70',
-                url: 'https://doctoraux.com/'
+                url: 'https://mathstutor.life/apps/roblox-corporation/5349/roblox.html'
             },
             {
                 logo: 'https://cdn.now.gg/assets-opt/_next/image?url=https%3A%2F%2Fcdn.now.gg%2Fassets-opt%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fcdn.now.gg%252Fapps-content%252Fcom.miHoYo.GenshinImpact%252Ficon%252Fgenshin-impact.png%26w%3D256%26q%3D80&w=1440&q=70',
